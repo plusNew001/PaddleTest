7f9098bfe85e91d3af2a126654d2fb0ebb4fad18
Jiaxin Sui <95567040+plusNew001@users.noreply.github.com>
2026-01-05 11:40:21 +0800

    Update test_spinn.py

--- models/PaddleScience/CI/test_models/test_spinn.py
+++ models/PaddleScience/CI/test_models/test_spinn.py
@@ -55,7 +55,7 @@ def test_spinn_loss():
     """
     epoch_num = 10  # 迭代次数
     output_dir = "./helmholtz3d"  # 输出目录
-    base_loss = 50.69930  # 基准损失值
+    base_loss = 24.96435  # 基准损失值
     # CI机器更换为T4,base值暂时修改
     # base_loss = 36.0843  # 基准损失值
 
